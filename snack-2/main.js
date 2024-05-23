ab862f56d52d05a7c405beae3c237e76a540c18a
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-05-23 15:21:34 +0200

    add the snack-2

---
+++ snack-2/main.js
@@ -0,0 +1,12 @@
+/* 
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+*/
+
+const array = ['ciao', 'COME', 'stai', 'CUFFIE']
+
+const newArray = array.map((element) => {
+    return element.charAt(0).toUpperCase() + element.substr(1).toLowerCase()
+})
+
+console.log(newArray);
+
